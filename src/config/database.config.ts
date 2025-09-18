af9a0b00ed36896b31283c408a6c39a385126c18
AlexAc0226 <alexis.correa026@gmail.com>
2025-09-18 17:37:09 -0300

    Refactor configuración de base de datos para usar await en la obtención de MONGO_URI y mejorar la legibilidad

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,8 +4,14 @@ import { ConfigService } from '@nestjs/config';
 export const databaseConfig = {
   useFactory: async (
     configService: ConfigService,
-  ): Promise<MongooseModuleOptions> => ({
-    uri: configService.get<string>('MONGO_URI'),
-  }),
+  ): Promise<MongooseModuleOptions> => {
+    const uri = await configService.get<string>('MONGO_URI');
+    return {
+      uri,
+      // Opciones para escalabilidad
+      maxPoolSize: 10, // Máximo de conexiones simultáneas
+      autoIndex: true, // Crea índices automáticamente en desarrollo
+    };
+  },
   inject: [ConfigService],
 };
